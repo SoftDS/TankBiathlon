39b2ab396af3cb7ab4a1ab0a46a54f3ab7ed38d3
Alexey <myfckndelirium@gmail.com>
2013-11-18 13:18:39 +0600

    add firingline massive

--- src/Distance.java
+++ src/Distance.java
@@ -1,13 +1,16 @@
 
 public class Distance {
 	private int amountStage;					//Количество этапов
+	private int amountFL = amountStage;         //Количество рубежей
 	private Stage stage[];
+	private FiringLine FL[];
 	
 	public Distance(){
 		
 	}
 	public Distance(int amountStage){
 		stage = new Stage[amountStage];
+		FL = new FiringLine[amountFL];
 	}
 	
 	public void createStage(){
