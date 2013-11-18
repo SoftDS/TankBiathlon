c35f9dce5d72020db0acc6a2a080104065203821
damn364 <damn364@gmail.com>
2013-11-18 16:11:02 +0600

    rename Distance to Map

---
+++ src/Map.java
@@ -0,0 +1,22 @@
+
+public class Map {
+	private int amountStage;		//Количество этапов
+	private int amountFL;			//Количество рубежей
+	private Stage stage[];
+	private FiringLine FL[];
+	
+	public Map(){
+		
+	}
+	public Map(int amountStage){
+		amountFL = amountStage;
+		stage = new Stage[amountStage];
+		FL = new FiringLine[amountFL];
+		
+	}
+	
+	public void createStage(){
+		
+	}
+	
+}
